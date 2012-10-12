e993444e366de9a41f01743bbdbca08606a1c277
Michal Terepeta <michal.terepeta@gmail.com>
2012-10-12 12:56:51 +0200

    ref_counted: initial import

---
+++ ref_counted.hh
@@ -0,0 +1,55 @@
+#ifndef REF_COUNTED_HH
+#define REF_COUNTED_HH
+
+#include <cstddef>
+#include <utility>
+
+#include <boost/intrusive_ptr.hpp>
+
+template <typename A, typename... Args>
+boost::intrusive_ptr<A> make_intrusive(Args&& ...args) {
+  return boost::intrusive_ptr<A>(new A(std::forward<Args>(args)...));
+}
+
+template <typename A>
+class RefCounted {
+  public:
+    std::size_t Count() const { return ref_count_; }
+
+    friend inline void intrusive_ptr_add_ref(const A *a) {
+      ++a->ref_count_;
+    }
+
+    friend inline void intrusive_ptr_release(const A *a) {
+      --a->ref_count_;
+      if(a->ref_count_ == 0) {
+        delete a;
+      }
+    }
+
+  protected:
+    /* All constructors should initialize the ref_count_ to zero.  Even the copy
+     * constructors -- we create a new copy of the object and nothing points to
+     * it. */
+
+    RefCounted() : ref_count_(0) { }
+
+    RefCounted(const RefCounted &rc) : ref_count_(0) { }
+
+    RefCounted(RefCounted &&rc) : ref_count_(0) { }
+
+    ~RefCounted() = default;
+
+    /* Assignment should be a nop -- it changes the contents but not the number
+     * of pointers that point to the object.  This is only important for derived
+     * classes that have automatically generated operator=. */
+
+    RefCounted &operator=(const RefCounted &rc) { return *this; }
+
+    RefCounted &operator=(RefCounted &&rc) { return *this; }
+
+  private:
+    mutable std::size_t ref_count_;
+};
+
+#endif /* REF_COUNTED_HH */
---
+++ ref_counted_example.cc
@@ -0,0 +1,52 @@
+#include "ref_counted.hh"
+
+struct Foo : public RefCounted<Foo> {
+  Foo(int i) : x(i) { }
+
+  void Debug() {
+    std::cout << this << " : " << x << " : " << RefCounted::Count() << std::endl;
+  }
+
+  int x;
+};
+
+
+int main() {
+  std::cout << "- 1" << std::endl;
+
+  boost::intrusive_ptr<Foo> foo = make_intrusive<Foo>(100);
+  foo->Debug();
+
+  std::cout << "- 2" << std::endl;
+
+  boost::intrusive_ptr<Foo> bar = foo;
+  foo->Debug();
+  bar->Debug();
+
+  std::cout << "- 3" << std::endl;
+
+  bar = make_intrusive<Foo>(*foo);
+  ++bar->x;
+  foo->Debug();
+  bar->Debug();
+
+  std::cout << "- 4" << std::endl;
+
+  boost::intrusive_ptr<Foo> foobar = make_intrusive<Foo>(300);
+
+  *foobar = *foo;
+  foo->Debug();
+  bar->Debug();
+  foobar->Debug();
+
+  std::cout << "- 5" << std::endl;
+
+  foobar = foo;
+  foo->Debug();
+  bar->Debug();
+  foobar->Debug();
+
+  // not possible:
+  // std::cout << foo->ref_count_ << std::endl;
+}
+
