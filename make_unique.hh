f56c89862d1cd88fedc7231c0d3431735ed38874
Michal Terepeta <michal.terepeta@gmail.com>
2012-10-12 12:57:16 +0200

    make_unique: add a missing include

--- make_unique.hh
+++ make_unique.hh
@@ -1,6 +1,8 @@
 #ifndef MAKE_UNIQUE_HH
 #define MAKE_UNIQUE_HH
 
+#include <utility>
+
 template <typename T, typename... Args>
 std::unique_ptr<T> make_unique(Args&& ...args) {
   return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
